56dda2311d690036e37b02657142254b8727ef39
nrossetti <nicholasjrossetti@gmail.com>
2019-01-10 22:13:00 -0600

    devboard revision
    added basic manual control with a option via serial command to
    enable/disable

--- rgb.ino
+++ rgb.ino
@@ -1,21 +1,31 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN            4
-#define NUMPIXELS      1
+#define LEDPIN             4
+#define NUMPIXELS       13
+#define delayTime       1
+#define minDelay        600
 
-bool newData = true;
-int color[] = {255, 255, 255};
 char inData[64];
 char inChar=-1;
+bool newData = true;
+bool enableTap = true;
+int val;
+int tickCount = 0;
+int color[] = {0, 75, 75, 1};
+int manual_colors[3][3] = {{100,0,0},{0,100,0},{0,0,0}};
+int manual_index = 0;
 
 char *token, *str, *tofree;
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
   Serial.begin(9600);
   pixels.begin(); // This initializes the NeoPixel library.
-  setColor(color);
+  for( int i = 1; i<(NUMPIXELS); i++)
+  {
+      pixels.setPixelColor(i, pixels.Color(color[1], color[0], color[2])); // Moderately brightn white color.
+  }   
   getData();
   procData();
   setColor(color);
@@ -23,30 +33,56 @@ void setup()
 
 void loop()
 {
-  getData();
-  procData();
-  setColor(color);
+getData();
+val = analogRead(A3);
+if(enableTap)
+  {
+  if(tickCount>minDelay)
+    if(val>150)
+    {
+      for( int i = 1; i<(NUMPIXELS); i++)
+      {
+        pixels.setPixelColor(i, pixels.Color(manual_colors[manual_index][1], manual_colors[manual_index][0], manual_colors[manual_index][2])); // 
+      } 
+      pixels.show();
+      if(manual_index>1)
+        manual_index=0;
+      else
+        manual_index++;
+      if(val>150)
+        delay(minDelay);
+      tickCount=0;  
+    }
+  tickCount++;
+  }
 }
 
 
-char getData()
+void getData()
 {
   byte numBytesAvailable= Serial.available();
     // if there is something to read
-    if (numBytesAvailable > 0){
+    if (numBytesAvailable > 0)
+    {
         // store everything into "inData"
         int i;
-        for (i=0;i<numBytesAvailable;i++){
+        for (i=0;i<numBytesAvailable;i++)
+        {
             inChar= Serial.read();
+            if(inChar='m')
+            {
+              enableTap=(!enableTap);
+              return;
+            }
             inData[i] = inChar;
         }
 
         inData[i] = '\0';
 
-        Serial.print("Arduino Received: ");
-        Serial.print("RGB(");
-        Serial.print(inData);
-        Serial.println(")");
+        //Serial.print("Arduino Received: ");
+        //Serial.println(inData);
+        procData();
+        delay(delayTime);
   }
 }
 
@@ -54,7 +90,7 @@ void procData()
 {
   int i=0;
   tofree = str = strdup(inData);  // We own str's memory now.
-  while (token = strsep(&str, ","))
+  while (token = strsep(&str, " "))
   {
     color[i]=atoi(token);
     //Serial.println(color[i]);
@@ -62,11 +98,14 @@ void procData()
   }
   free(tofree);
   memset(inData, 0, sizeof(inData));
+  setColor(color);
 }
 
 void setColor(int color[])
 {
-  pixels.setPixelColor(0, pixels.Color(color[1], color[0], color[2])); // Moderately brightn white color.
-  pixels.show(); // This sends the updated pixel color to the hardware. 
+    //for( int i = 1; i<(NUMPIXELS); i++){
+      pixels.setPixelColor(color[3], pixels.Color(color[1], color[0], color[2])); // Moderately brightn white color.
+    //}   
+  pixels.show();
 }
 
